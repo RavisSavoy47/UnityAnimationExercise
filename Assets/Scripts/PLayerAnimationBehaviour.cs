78caebb939aba683f7a6772aeffbe389497739ac
ravis <ravissavoy49@gmail.com>
2022-05-25 16:37:40 -0500

    create a full animation idle walk run and inair

---
+++ Assets/Scripts/PLayerAnimationBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerAnimationBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerMovementBehaviour _playermovement;
+    [SerializeField]
+    private Animator _animator;
+
+    private void Awake()
+    {
+        WinBoxBehaviour.OnWin += () => _animator.SetTrigger("Win");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetFloat("Speed", _playermovement.Velocity.magnitude);
+
+        _animator.SetBool("IsGrounded", _playermovement.IsGrounded);
+    }
+}
